13928bf4f4a1b928bb5db6e1634df25b1d2474ed
Patryk <106694396+Hypereqqq@users.noreply.github.com>
2023-11-23 15:57:13 +0100

    Update ramachandran.py

--- zad2b/ramachandran.py
+++ zad2b/ramachandran.py
@@ -5,7 +5,7 @@ from matplotlib.lines import Line2D
 
 sciezka = "/home/markowar/bios/1prr.pdb"
 p = PDBParser()
-strukura=p.get_strukura("1MBO",sciezka)
+strukura=p.get_structure("1MBO",sciezka)
 print(strukura)
 
 for item in strukura.get_models():
@@ -18,7 +18,7 @@ dssp = DSSP(model,sciezka,dssp='/usr/bin/dssp')
 kolory = {'H': 'red','B':'orange','E':'yellow', 'G':'blue','I':'violet','T': 'pink','S':'green', '-': 'black'}
 for amino in list(dssp.keys()):
     plt.scatter(dssp[amino][4], dssp[amino][5], c=kolory[dssp[amino][2]],alpha=0.7)
-legend_elements = [
+legenda = [
     Line2D([0], [0], marker='o', color='w', markerfacecolor='red', markersize=10, label='Alfa helisa'),
     Line2D([0], [0], marker='o', color='w', markerfacecolor='orange', markersize=10, label='Mostek beta'),
     Line2D([0], [0], marker='o', color='w', markerfacecolor='yellow', markersize=10, label='Nić'),
@@ -26,10 +26,10 @@ legend_elements = [
     Line2D([0], [0], marker='o', color='w', markerfacecolor='violet', markersize=10, label='Pi helisa'),
     Line2D([0], [0], marker='o', color='w', markerfacecolor='pink', markersize=10, label='Turn'),
     Line2D([0], [0], marker='o', color='w', markerfacecolor='green', markersize=10, label='Bend'),
-    Line2D([0], [0], marker='o', color='w', markerfacecolor='black', markersize=10, label='None')
+    Line2D([0], [0], marker='o', color='w', markerfacecolor='black', markersize=10, label='Brak')
 ]
 
-plt.legend(handles=legend_elements)
+plt.legend(handles=legenda)
 plt.title('Wykres Ramachandrana')
 plt.xlabel('Wartość Phi')
 plt.ylabel('Wartość Psi')
